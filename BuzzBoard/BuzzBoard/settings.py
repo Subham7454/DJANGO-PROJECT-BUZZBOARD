1900f22e35757f24faa161446a79d0379cef945a
Subham <shubhamrawat1843@gmail.com>
2024-11-20 11:44:39 +0530

    hosting commit

--- BuzzBoard/BuzzBoard/settings.py
+++ BuzzBoard/BuzzBoard/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m691xrq4$&0!m4g)r#f+&ropzc7=n_sef(an+jc$d$g&ryzq)q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
